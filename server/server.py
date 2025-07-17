da42f16e9a4252b720dd79ea401b505b34e8d16a
Rohit Maurya <102695143+rohit8168@users.noreply.github.com>
2025-07-17 08:32:36 +0530

    Update server.py
    cors

--- server/server.py
+++ server/server.py
@@ -11,7 +11,7 @@ import threading
 
 load_dotenv()
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000,https://rohit-assignment-fvoo.vercel.app/,https://rohit-assignment-alpha.vercel.app/"}})
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 def scrape_hacker_news_task():
     page = 1  
